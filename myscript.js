af3fc84f7fb2301218f01cf7c64f84495049baf3
Prety <rkive.arch@gmail.com>
2024-03-14 15:32:12 +0800

    commit devops

---
+++ myscript.js
@@ -0,0 +1,9 @@
+// Turn On
+function turnOn(){
+    document.getElementById('myImage').src='pic_on.gif';
+}
+
+// Turn Off
+function turnOff(){
+    document.getElementById('myImage').src='pic_off.gif';
+}
